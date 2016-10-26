9fad84664930ba114d0bf1585cd4fcd07ab4ff29
Robin Elahi <elahi.robin@gmail.com>
2016-10-26 14:56:21 -0700

    checked temporal trend stats

--- 06_analyse_temp_time_series.R
+++ 06_analyse_temp_time_series.R
@@ -25,20 +25,24 @@ source("R/get_gls_results.R")
 
 ##### TRENDS IN AIR ANNUAL MEANS #####
 
+crap <- gls(maximum ~ year, data = air_annual, correlation = corAR1())
+summary(crap)$tTable
+anova(crap)
+
 gls_annual_summary <- air_annual_long %>% group_by(metric) %>% 
   do(get_gls_results(df = ., dep_var = "tempC", ind_var = "year")) %>%
   ungroup()
 
 gls_annual_summary2 <- gls_annual_summary %>% 
   filter(coef_name == "slope") %>% 
-  select(-coef_name, -t_value) %>% 
+  select(-coef_name) %>% 
   rename(slope = value)
 
 gls_time_series_air <- gls_annual_summary2
 
 gls_time_series_air
 
-# write.csv(gls_time_series_air, "output/gls_time_series_air.csv")
+write.csv(gls_time_series_air, "output/gls_time_series_air.csv")
 
 ##### TRENDS IN SST ANNUAL MEANS #####
 
@@ -48,14 +52,14 @@ gls_annual_summary <- sst_annual_long %>% group_by(metric) %>%
 
 gls_annual_summary2 <- gls_annual_summary %>% 
   filter(coef_name == "slope") %>% 
-  select(-coef_name, -t_value) %>% 
+  select(-coef_name) %>% 
   rename(slope = value)
 
 gls_time_series_sst <- gls_annual_summary2
 
 gls_time_series_sst
 
-#write.csv(gls_time_series_sst, "output/gls_time_series_sst.csv")
+write.csv(gls_time_series_sst, "output/gls_time_series_sst.csv")
 
 
 
