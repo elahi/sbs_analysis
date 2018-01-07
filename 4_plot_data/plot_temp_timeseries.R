67793e39a3875b8cc1713721e3e9b88774483394
Robin Elahi <elahi.robin@gmail.com>
2018-01-07 10:45:04 -0800

    modified temperature summary script

--- 4_plot_data/plot_temp_timeseries.R
+++ 4_plot_data/plot_temp_timeseries.R
@@ -9,30 +9,8 @@
 ##' @log 
 ################################################################################
 
-#rm(list=ls(all=TRUE)) 
-
-# Get monterey air summary
-source("2_summarise_data/summarise_monterey_weather.R")
-air_annual_long <- air_annual_long %>% 
-  mutate(dataset = "Air")
-
-# Get SST summary
-source("2_summarise_data/05_summarise_hms_sst.R")
-sst_annual_long
-sst_annual_long <- sst_annual_long %>% 
-  mutate(dataset = "Seawater")
-
-### Load limpet hindcasts (15 yrs of modeled body temperatures)
-source("05_summarise_limpet_temps.R")
-lim_annual_seasons_long2
-
-theme_set(theme_bw(base_size = 12))
-
-# theme_set(theme_bw(base_size = 12) + 
-#             theme(panel.grid = element_blank()))
-
-### Combine data
-temp_records <- rbind(sst_annual_long, air_annual_long)
+source("2_summarise_data/summarise_temperature.R")
+annual_df_long
 
 ##### GET YEARS OF SNAIL SAMPLES #####
 head(dat4)
@@ -116,59 +94,3 @@ temp_records %>%
 
 ggsave("figs/temp_timeseries.png", height = 4.5, width = 7)
   
-##### TWO-PANEL PLOT - FOR PPT #####
-
-sort(unique(lim_annual_seasons_long2$tidalHT))
-
-lim_for_sst <- lim_annual_seasons_long2 %>% 
-  filter(tidalHT == 1.1) %>% 
-  mutate(dataset = "Seawater")
-
-lim_for_air <- lim_annual_seasons_long2 %>% 
-  filter(tidalHT == 7.55) %>% 
-  mutate(dataset = "Air")
-
-text_df <- data.frame(x = c(rep(1940, 3)), 
-                      y = c(6.5, 13, 21), 
-                      text1 = c("Min", "Median", "Max"), 
-                      dataset = rep("Air", 3), 
-                      metric = c("minimum", "median", "maximum"))
-
-facet_label_text <- data.frame(x = rep(1938, 2), 
-                               y = rep(23.3, 2), 
-                               text1 = c("A", "B"), 
-                               dataset = c("Air", "Seawater"))
-
-temp_records %>% 
-  ggplot(aes(year, tempC, color = metric)) + 
-  geom_smooth(data = subset(temp_records, dataset == "Seawater" & 
-                              metric == "maximum"), method = "lm", 
-              color = "darkgray", linetype = "dashed") + 
-  # geom_smooth(data = subset(temp_records, dataset == "Air" & 
-  #                             metric == "maximum"), method = "lm", 
-  #             color = "darkgray", linetype = "dashed") + 
-  geom_smooth(data = subset(temp_records, dataset == "Air" & 
-                              metric == "minimum"), method = "lm", 
-              color = "darkgray", linetype = "dashed") + 
-  geom_line(alpha = 1) +
-  ylab(expression(paste("Temperature (", degree, "C)"))) + 
-  xlab("Year") + 
-  theme(legend.title = element_blank()) + 
-  theme(legend.position = "top") + 
-  guides(color = FALSE) + 
-  guides(shape = 
-           guide_legend(title = "SAMPLING EVENT", title.position = "top", 
-                        title.hjust = 0.5, 
-                        title.theme = 
-                          element_text(size = 10, face = "bold", angle = 0), 
-                        direction = "horizontal", 
-                        label.theme = element_text(angle = 0, size = 8, 
-                                                   face =  "italic"))) + 
-  facet_wrap(~ dataset) + 
-  theme(strip.background = element_blank()) + 
-  geom_text(aes(x, y, label = text1), data = text_df, size = 3, 
-            hjust = 0, fontface = "bold") 
-
-
-ggsave("figs/temp_timeseries_ppt.png", height = 3.5, width = 7)
-
