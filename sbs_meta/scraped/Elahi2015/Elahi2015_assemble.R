5051c97363849d328f1bd13ded3e5d4364439f84
Robin Elahi <elahi.robin@gmail.com>
2018-02-18 13:47:13 -0800

    scraped treneman and elahi

--- sbs_meta/scraped/Elahi2015/Elahi2015_assemble.R
+++ sbs_meta/scraped/Elahi2015/Elahi2015_assemble.R
@@ -9,9 +9,8 @@
 ##' 
 ##' @log 
 ##' 2016-08-02: added summary stats for 50% cutoff - max size
+##' 2018-02-18: updated methods (e.g., uniform distribution for sizes)
 ################################################################################
-
-rm(list=ls(all=TRUE)) 
 
 ##### LOAD PACKAGES, DATA #####
 
@@ -22,37 +21,91 @@ library(ggplot2)
 theme_set(theme_bw(base_size = 12))
 library(lubridate)
 
-# load cleaned up data
-source("03_summarise_size_data.R")
-head(dm2)
+# Function to load cleaned data
+source("R/choose_size_data.R")
+
+# load data - approximated sizes
+dat <- choose_size_data(method = "uniform")
+names(dat)
+unique(dat$sampleArea)
+unique(dat$site)
+
+# Fix site for wara
+dat <- dat %>% 
+  mutate(site = ifelse(sp == "LIKE", as.character(sampleArea), site))
+
+# Exclude smallest snails?
+dat %>% 
+  ggplot(aes(size1mm, fill = era)) + 
+  geom_histogram(binwidth = 1) + 
+  facet_wrap(~ species) + 
+  geom_vline(xintercept = 5)
+
+min_threshold <- 0
+
+dat2 <- dat %>% filter(size1mm >= min_threshold)
+
+##### SUMMARISE #####
+
+## Summarise by site
+dat_summary <- dat2 %>% 
+  group_by(species, sp, site, year, era) %>% 
+  summarise(size1mm_mean = mean(size1mm, na.rm = TRUE), 
+            size1mm_sd = sd(size1mm, na.rm = TRUE), 
+            sample_size = n()) %>%
+  ungroup() 
+
+dat_summary
+
+#### LAT LONG ####
+
+ll_dat <- dat %>% 
+  filter(!is.na(lat)) %>% 
+  group_by(species, site) %>% 
+  summarise(lat_mean = mean(lat), 
+            long_mean = mean(long)) %>% 
+  ungroup()
+ll_dat
+
+dat_summary <- left_join(dat_summary, ll_dat, by = c("species", "site"))
 
 ##### FORMAT TABLE FOR META-ANALYSIS #####
-names(dm2)
+names(dat_summary)
 
-df_final <- dm2  %>%
-  rename(size_rep = size_mean, size_error = size_CI, 
-         sample_size = size_n) %>%
+df_final <- dat_summary  %>%
+  mutate(size_rep = size1mm_mean, 
+         size_error = size1mm_sd, 
+         sample_size_units = "number of snails", 
+         time_rep = NA,          
+         size_original = "raw") %>% 
   arrange(species, site, year)
 
 head(df_final)
 
+
 dfMeta <- data.frame(
-  study = "Elahi2015", 
-  studySub = df_final$studySub, 
-  fig_table = "raw_data", 
+  study = "Elahi_2015", 
+  studySub = NA, 
+  fig_table = "various", 
   species = df_final$species, 
-  site = df_final$sampleArea, 
+  site = df_final$site, 
+  size_original = df_final$size_original, 
   size_rep = df_final$size_rep, 
   size_units = "mm", 
   size_error = df_final$size_error, 
-  size_error_type = "CI", 
-  time_rep = NA, 
+  size_error_type = "SD", 
+  time_rep = df_final$time_rep, 
   time_error = NA, 
+  era = df_final$era, 
   year = df_final$year, 
   year_error = NA, 
   year_error_type = NA, 
   sample_size = df_final$sample_size, 
-  sample_size_units = "number of snails"
+  sample_size_units = df_final$sample_size_units, 
+  museum = FALSE, 
+  size_threshold_mm = 0, 
+  latitude = lat_mean, 
+  longitude = long_mean
 )
 
 write.csv(dfMeta, "sbs_meta/output/dfMeta_Elahi2015.csv")
---
+++ sbs_meta/scraped/Galloway_2017/Galloway-Treneman_assemble.R
@@ -0,0 +1,120 @@
+################################################################################
+##' @title Assembling scraped data, Galloway-Treneman 2017
+##'
+##' @author Robin Elahi
+##' @contact elahi.robin@gmail.com
+##' @contributor 
+##' 
+##' @date 2018-02-18
+##' 
+##' @log Add a log here
+################################################################################
+
+##### LOAD PACKAGES, DATA #####
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+
+tren_past <- read_csv("sbs_meta/scraped/Galloway_2017/Galloway-Treneman_SunsetBay_Limpets_1979.csv")
+summary(tren_past)
+
+tren_pres <- read_csv("sbs_meta/scraped/Galloway_2017/Galloway-Treneman_SunsetBay_Limpets_2017.csv")
+
+tren_past %>% 
+  ggplot(aes(1, ave.size.mm)) + geom_boxplot()
+
+tren_pres %>% 
+  ggplot(aes(tide.ht.m, size.mm)) + 
+  geom_point(alpha = 0.5) + 
+  facet_wrap(~ species) + 
+  geom_point(data = tren_past, aes(tide.ht.m, ave.size.mm), color = "red", alpha = 0.5)
+
+##### SUMMARISE DATA #####
+## Get means by tidal height for present data
+tren_pres_summary <- tren_pres %>% 
+  group_by(species, sp, tide.ht.m, site) %>% 
+  summarise(size1mm = mean(size.mm, na.rm = TRUE)) %>%
+  ungroup() %>% 
+  mutate(year = 2017, 
+         era = "present")
+
+## format tren_past
+tren_past2 <- tren_past %>% 
+  select(species, sp, tide.ht.m, ave.size.mm, site) %>% 
+  rename(size1mm = ave.size.mm) %>% 
+  mutate(year = 1979, 
+         era = "past")
+
+## Combine mean size for past and present
+tren_summary <- rbind(tren_past2, tren_pres_summary)
+
+tren_summary %>% 
+  ggplot(aes(era, size1mm)) + 
+  geom_boxplot() + 
+  facet_wrap(~ species)
+
+## In Treneman's original paper, she mentioned that it was difficult to identify limpet species when below 5mm
+## Use 5mm as a threshold
+
+min_threshold <- 5
+
+## Summarize for meta-analysis
+tren_meta <- tren_summary %>% 
+  filter(!is.na(size1mm)) %>% 
+  filter(size1mm >= min_threshold) %>% 
+  group_by(species, sp, year, era, site) %>% 
+  summarise(size_mean = mean(size1mm), 
+            size_sd = sd(size1mm), 
+            n = n()) %>% 
+  ungroup() %>% 
+  mutate(size_original = "mean")
+tren_meta
+
+##### GET LAT LONGS #####
+
+lat_mean = 43.3336
+long_mean = -124.3769
+
+##### FORMAT TABLE FOR META-ANALYSIS #####
+names(tren_meta)
+
+df_final <- tren_meta %>% 
+  rename(size_rep = size_mean, size_error = size_sd, 
+         sample_size = n) %>%
+  mutate(species = gsub(species, pattern = "\\.", replacement = " "), 
+         time_rep = NA, 
+         sample_size_units = "mean size of snails by tidal height") %>%
+  arrange(species, year)
+
+head(df_final)
+
+dfMeta <- data.frame(
+  study = "Galloway-Treneman_2017", 
+  studySub = NA, 
+  fig_table = "Figure_2", 
+  species = df_final$species, 
+  site = df_final$site, 
+  size_original = df_final$size_original, 
+  size_rep = df_final$size_rep, 
+  size_units = "mm", 
+  size_error = df_final$size_error, 
+  size_error_type = "SD", 
+  time_rep = df_final$time_rep, 
+  time_error = NA, 
+  era = df_final$era, 
+  year = df_final$year, 
+  year_error = NA, 
+  year_error_type = NA, 
+  sample_size = df_final$sample_size, 
+  sample_size_units = df_final$sample_size_units, 
+  museum = FALSE, 
+  size_threshold_mm = 5, 
+  latitude = lat_mean, 
+  longitude = long_mean
+)
+
+dfMeta
+
+write.csv(dfMeta, "sbs_meta/output/dfMeta_Galloway-Treneman_2017.csv")
