244e276dca48c37bf9c449aa050dcfb76711a084
Robin Elahi <elahi.robin@gmail.com>
2018-02-21 20:59:30 -0800

    revised meta lrr plot

--- sbs_meta/01_assemble_metafor_data.R
+++ sbs_meta/01_assemble_metafor_data.R
@@ -43,23 +43,36 @@ roy_presentCNM <- roy %>% filter(studySub != "Present") %>%
 
 ## Compile
 dat <- rbind(elahi, gall_frank, gall_tren, hay_king, fisher, wilson, roy_present, roy_presentCNM) 
+### Add UK to lapillus
+dat <- dat %>% 
+  mutate(species2 = ifelse(study == "Wilson-Brodie 2017", 
+                           paste(species, "(UK)", sep = " "), species), 
+         species2 = ifelse(study == "Galloway-Frank_2017", 
+                           paste(species, "(OR)", sep = " "), species2))
 
 ## For metafor, I want the data in wide format
 names(dat)
 dat2 <- dat %>% 
-  select(study, species, museum, latitude, era, size_rep, size_error, sample_size) %>% 
+  select(study, species, species2, museum, latitude, era, size_rep, size_error, sample_size) %>% 
   rename(size_n = sample_size)
 dat2
 
 datM <- dat2 %>% 
   gather(key = temp, value = value, starts_with("size")) %>% 
   unite(col = temp1, era, temp, sep = "_") %>% 
-  spread(temp1, value)
+  spread(temp1, value) %>% 
+  arrange(desc(latitude))
+datM
+
+### Re-order by latitude
+datM <- datM %>% 
+  mutate(species2 = reorder(species2, latitude))
 
 ## Try metafor
 library(metafor)
 names(datM)
-names(datM) <- c("study","species", "museum", "latitude", "sd2i", "n2i", "m2i", "sd1i", "n1i", "m1i")
+names(datM) <- c("study", "species", "species2", "museum", "latitude", 
+                 "sd2i", "n2i", "m2i", "sd1i", "n1i", "m1i")
 datM
 
 ### calculate log ratio of means and corresponding sampling variances
@@ -73,15 +86,22 @@ datM <- datM %>%
          upper = yi + 2*sei, 
          lower = yi - 2*sei)
 
-### Re-order by latitude
-datM <- datM %>% 
-  mutate(species = reorder(species, latitude))
-
 ### Plot this
-my_dodge <- 0.5
+my_dodge <- 0
 
-datM %>% 
-  ggplot(aes(species, yi, fill = study, shape = museum)) + 
+spp_df <- datM %>% distinct(species2, study)
+spp_df
+spp_df <- spp_df %>% 
+  mutate(my_text = c(rep("Central California", 3), 
+                     seq(1:13)))
+glimpse(spp_df)
+
+my_axis_labels <- paste(datM$species)
+my_axis_labels
+rev(my_axis_labels)
+
+p <- datM %>% 
+  ggplot(aes(species2, yi, fill = study, shape = museum)) + 
   geom_point(position = position_dodge(my_dodge), size = 3) + 
   geom_errorbar(aes(ymin = lower, ymax = upper), 
                 position = position_dodge(my_dodge), 
@@ -90,11 +110,22 @@ datM %>%
   geom_hline(yintercept = 0, linetype = "dashed", color = "gray") + 
   labs(y = "Proportional change in body size", 
        x = "") + 
-  scale_shape_manual(values = c(21, 22))
+  scale_shape_manual(values = c(21, 22)) + 
+  theme(legend.position = "none") + 
+  #scale_x_discrete("", labels = rev(my_axis_labels)) + 
+  theme(axis.text.y = element_text(face = "italic")) 
+p
+
+# # Code to override clipping
+# library(grid)
+# gt <- ggplot_gtable(ggplot_build(p))
+# gt$layout$clip[gt$layout$name == "panel"] <- "off"
+# grid.draw(gt)
+
 ggsave("sbs_meta/meta_figs/meta_lrr.pdf", height = 5, width = 7)
 
 ### meta-analysis of log ratio of means using a random-effects model
 res <- rma(yi, vi, method = "DL", data = datM)
 res
-
+forest(res)
 
