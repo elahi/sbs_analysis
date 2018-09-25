32aec8e29fe29ba15015876325d28a112e39d0d1
Robin Elahi <elahi.robin@gmail.com>
2018-09-25 13:31:09 -0700

    create meta file for hayford elahi

--- sbs_meta/scraped/HayfordElahi_2018/Hayford-Elahi_assemble.R
+++ sbs_meta/scraped/HayfordElahi_2018/Hayford-Elahi_assemble.R
@@ -21,33 +21,7 @@ source("R/choose_size_threshold.R")
 
 # Size-frequency raw data, Hayford-Elahi-FHL470, 2018
 he <- read_csv("sbs_meta/scraped/HayfordElahi_2018/Shady_Cove_shelllength_rawdata_2018.csv")
-he
-
-# Sampling began when the tide height was -0.15m below MLLW, I use 0 below. 
-
-he_present <- data.frame(
-  study = "Hayford-Elahi", 
-  studySub = NA, 
-  species = "Nucella lamellosa", 
-  sp = "NULA", 
-  site = "ShadyCove",
-  era = "present", 
-  date = "4/19/2018", 
-  nest1 = NA, 
-  nest2 = NA, 
-  nest3 = NA, 
-  nest3note = NA, 
-  sampleUnit = NA, 
-  size1mm = he$shell_length_mm, 
-  size1mm_rand = he$shell_length_mm, 
-  habitat = NA, 
-  tideHTm = 0, 
-  lat = 48.552,  
-  long = -123.006, 
-  Shaw_hab = NA, 
-  notes = "", 
-  notes2 = ""
-)
+he <- he %>% rename(size1mm = shell_length_mm, breeding = breeding_group, year = Year)
 
 # Size-frequency counts, Spight 1968 at Shady Cove
 # Spight 1974, Ecology, Figure 2 Population IV
@@ -60,100 +34,101 @@ spight <- spight %>%
   mutate(size_bin = round(shell_length_mm, 0), 
          frequency_of_occurence = round(frequency_of_occurence, 0), 
          yr_bg = paste(Year, breeding_group, sep = "_"))
-spight %>% count(Year, breeding_group, size_bin) %>% print(n = 100)
-
-# Breeding group is irrelevant, sum across size_bin
-spight_sum <- spight %>%
-  group_by(Year, size_bin) %>% 
-  summarise(count = sum(frequency_of_occurence)) %>%
-  ungroup()
 
 # Use repeated sizes, because increments are in 1mm for both past and present
+# Need a loop to cycle through each year and breeding group to repeat the observed sizes
 yr_bg_unique <- unique(spight$yr_bg)
-past_size <- vector()
-past_size
-i = 1
+past_size_df <- data.frame(size1mm = NA, 
+                           year = NA, 
+                           breeding = NA)
 
+# Run loop
 for(i in 1:length(yr_bg_unique)){
   
   dat_i <- spight %>% filter(yr_bg == yr_bg_unique[i])
+  year_i <- unique(dat_i$Year)
+  breeding_i <- unique(dat_i$breeding_group)
   past_size_i <- repeat_sizes(size_bin_vector = dat_i$size_bin,
                               count_vector = dat_i$frequency_of_occurence)
   
+  past_size_df_i <- data.frame(size1mm = past_size_i, 
+                             year = year_i, 
+                             breeding = breeding_i)
+  
+  past_size_df <- rbind(past_size_df, past_size_df_i)
+  
 }
 
-past_size <- spight %>% 
-  group_by(Year, breeding_group) %>% 
-  do(., repeat_sizes(size_bin_vector = .$size_bin, 
-                     count_vector = .$frequency_of_occurence)) %>% 
-  ungroup()
-  
+past_size_df %>% count(breeding, year)
+past_size_df <- past_size_df %>% filter(!is.na(breeding))
 
-past_size <- repeat_sizes(size_bin_vector = spight$size_bin,
-                          count_vector = spight$count)
+# Combine past and present data
+he <- rbind(past_size_df, he) %>% 
+  mutate(population = "V_VI", 
+         province = "TS_SB_43S_UCV_CV", 
+         figure = "Spight_1974_Ecology_Fig2") 
 
-past_size_norm <- get_random_sizes(size_bin_vector = spight$size_bin,
-                                   count_vector = spight$count, 
-                                   size_interval = 1, 
-                                   distribution = "normal")
+head(he)
 
-past_size_unif <- get_random_sizes(size_bin_vector = spight$size_bin,
-                                   count_vector = spight$count, 
-                                   size_interval = 1, 
-                                   distribution = "uniform")
+write.csv(he, "sbs_meta/scraped/HayfordElahi_2018/Spight_snail_resurvey_180925.csv")
 
-hist(past_size, breaks = 25)
-hist(past_size_norm, breaks = 25)
-par(mfrow = c(1,2))
-hist(past_size_norm, breaks = 25)
-hist(past_size_unif, breaks = 25)
+##### FORMATTING DATA #####
 
-# Use repeated sizes, because increments are in 1mm for both past and present
+he2 <- he %>% 
+  mutate(date = ifelse(year == 2018, "4/19/2018", paste("4/15", year, sep = "/")), 
+         notes = ifelse(year == 2018, NA, "day is unknown"), 
+         era = ifelse(year > 2017, "present", "past")) %>% 
+  tbl_df()
+unique(he2$date)
+unique(he2$notes)
+he2
 
-he_past <- data.frame(
+# Sampling began when the tide height was -0.15m below MLLW, I use 0 below. 
+
+he3 <- data.frame(
   study = "Hayford-Elahi", 
   studySub = NA, 
   species = "Nucella lamellosa", 
   sp = "NULA", 
   site = "ShadyCove",
-  era = "past", 
-  date = "4/1/1968", 
+  era = he2$era, 
+  date = he2$date, 
   nest1 = NA, 
   nest2 = NA, 
   nest3 = NA, 
   nest3note = NA, 
   sampleUnit = NA, 
-  size1mm = past_size, 
-  size1mm_rand = past_size, 
+  size1mm = he2$size1mm, 
+  size1mm_rand = he2$size1mm, 
   habitat = NA, 
-  tideHTm = NA, 
-  lat = NA,  
-  long = NA, 
+  tideHTm = 0, 
+  lat = 48.552,  
+  long = -123.006, 
   Shaw_hab = NA, 
-  notes = "day is unknown", 
-  notes2 = ""
+  notes = he2$notes, 
+  notes2 = "", 
+  year = he2$year
 )
 
-he2 <- rbind(he_present, he_past)
-
-unique(he2$date)
-he2 <- he2 %>%
-  mutate(year = ifelse(era == "past", 1968, 2018))
-he2 %>% count(era)
-with(he2, plot(size1mm, size1mm_rand))
+head(he3)
 
 # Save this file
-write.csv(he2, "sbs_meta/scraped/HayfordElahi_2018/Hayford-Elahi_raw.csv")
+write.csv(he3, "sbs_meta/scraped/HayfordElahi_2018/Hayford-Elahi_raw.csv")
 
 ##### SUMMARISE #####
 
+# Select years to analyze
+he3 %>% count(year)
+dat <- he3 %>% filter(year == 1969 | year == 2018)
+
 # Size threshold for museum comparison
-dat <- he2 %>% mutate(size_threshold = 0)
+dat <- dat %>% mutate(size_threshold = 0)
 dat_sub <- choose_size_threshold_general(dat, era = "combined", my_quantile = 0.5)
 
 dat %>% count(species, era)
 dat_sub %>% count(species, era)
-## Summarise across sample areas
+
+## Summarise
 dat_summary_all <- dat %>% 
   group_by(species, sp, year, era, size_threshold) %>% 
   summarise(size1mm_mean = mean(size1mm, na.rm = TRUE), 
@@ -174,40 +149,25 @@ dat_summary_sub <- dat_sub %>%
 
 dat_summary <- rbind(dat_summary_all, dat_summary_sub)
 
-##### GET LAT LONGS #####
-str(frank2)
-
-remove_last_char <- function(t) substr(t, 1, nchar(t)-1)
-remove_last_char(frank2$lat)
-
-ll_dat <- frank2 %>% 
-  filter(era == "present") %>% 
-  mutate(lat = as.numeric(remove_last_char(as.character(lat))), 
-         long = -as.numeric(remove_last_char(as.character(long))))
-
-ll_dat <- ll_dat %>% 
-  summarise(lat_mean = mean(lat), 
-            long_mean = mean(long))
-
 ##### FORMAT TABLE FOR META-ANALYSIS #####
 df_final <- dat_summary %>% 
   mutate(size_rep = size1mm_mean, 
          size_error = size1mm_sd, 
          size_original = "raw", 
-         species = "Tegula funebralis", 
+         species = "Nucella lamellosa", 
          time_rep = NA, 
          sample_size_units = "total number of snails", 
-         lat = ll_dat$lat_mean, 
-         long = ll_dat$long_mean, 
-         site = "South Cove") %>%
+         lat = unique(dat$lat), 
+         long = unique(dat$long), 
+         site = "Shady Cove") %>%
   arrange(species, year)
 
 names(df_final)
 
 dfMeta <- data.frame(
-  study = "Galloway_2017", 
+  study = "Hayford-Elahi_2018", 
   studySub = df_final$studySub, 
-  fig_table = "Table_4", 
+  fig_table = "Figure 2", 
   species = df_final$species, 
   site = df_final$site, 
   size_original = df_final$size_original, 
@@ -223,7 +183,7 @@ dfMeta <- data.frame(
   year_error_type = NA, 
   sample_size = df_final$sample_size, 
   sample_size_units = df_final$sample_size_units, 
-  museum = df_final$museum, 
+  museum = FALSE, 
   size_threshold_mm = df_final$size_threshold, 
   latitude = df_final$lat, 
   longitude = df_final$long
@@ -231,4 +191,4 @@ dfMeta <- data.frame(
 
 dfMeta
 
-write.csv(dfMeta, "sbs_meta/output/dfMeta_Galloway-Frank_2017.csv")
+write.csv(dfMeta, "sbs_meta/output/dfMeta_Hayford-Elahi_2018.csv")
